1917f07d0995eccb6253231322a6edcbc17e7296
биг шмоук <iloveboobds@gmail.com>
2024-12-09 17:03:16 +0300

    Добавьте файлы проекта.

---
+++ Урок 1/main.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double one, two;
+	
+	one = 0;
+	two = 0;
+	std::cout << "¬ведите первое число:";
+	std::cin >> one;
+	std::cout << "¬ведите второе число:";
+	std::cin >> two;
+
+	std::cout << "—умма:" << one + two << "\n";
+
+
+
+
+
+
+
+
+
+
+
+	    
+
+
+
+
+		return 0;
+
+
+
+
+
+
+
+
+
+}
